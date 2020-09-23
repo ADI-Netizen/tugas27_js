e3d8fb716dfa61c62049d7e9d801a002021f409d
ADI-Netizen <71365604+ADI-Netizen@users.noreply.github.com>
2020-09-23 20:59:53 +0700

    Add files via upload

---
+++ tugas27.js
@@ -0,0 +1,10 @@
+var a = "No ";
+var b = "rumah saya ";
+var c = 30;
+var d = "A";
+var e = "Jalan Pangeran Tirtayasa No"
+var f = 70
+
+gabung = String(a) + String(b) + String(c) + String(d);
+console.log(gabung);
+console.log(gabung);
